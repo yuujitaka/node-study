6d46cf0f370751af920e3b4da79f282425dbc2c4
Cleviane Rebeca <rebecacleviane@gmail.com>
2023-07-19 15:50:05 +0200

    chaining file methods

--- index.js
+++ index.js
@@ -13,6 +13,33 @@ fs.readFile(
 //shown first
 console.log('hello...');
 
+fs.writeFile(
+  path.join(__dirname, 'files', 'reply.txt'),
+  'Nice to meet you',
+  (err) => {
+    if (err) throw err;
+    console.log('Write complete');
+
+    fs.appendFile(
+      path.join(__dirname, 'files', 'reply.txt'),
+      '\n\nyes it is!',
+      (err) => {
+        if (err) throw err;
+        console.log('Append complete');
+
+        fs.rename(
+          path.join(__dirname, 'files', 'reply.txt'),
+          path.join(__dirname, 'files', 'newReply.txt'),
+          (err) => {
+            if (err) throw err;
+            console.log('Rename complete');
+          }
+        );
+      }
+    );
+  }
+);
+
 process.on('uncaughtException', (err) => {
   console.error(`There was an uncaught error: ${err}`);
   process.exit(1);
