c35d6825767b3a1e3e7f308adc85632bd642bd78
Cleviane Rebeca <rebecacleviane@gmail.com>
2023-07-20 15:28:09 +0200

    adding scripts and libraries

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('testing');
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+console.log(format(new Date(), 'yyyy/MM/dd\tHH:mm:ss'));
+console.log(uuid());
