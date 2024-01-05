244cfa9bcd8459f78744391713a89b3f8e69ad71
Cleviane Rebeca <rebecacleviane@gmail.com>
2024-01-05 12:55:27 +0100

    employee handlers with mongoose

--- controllers/employeesController.js
+++ controllers/employeesController.js
@@ -1,30 +1,64 @@
-const data = require('../model/employees.json');
+const Employee = require('../model/Employee');
 
 //Route handlers:https://expressjs.com/en/guide/routing.html
-const getAllEmployees = (req, res) => {
-  res.json(data);
+const getAllEmployees = async (req, res) => {
+  const employees = await Employee.find();
+
+  if (employees) return res.status(204).json({ message: 'No employee found' });
+
+  res.json(employees);
 };
 
-const getEmployee = (req, res) => {
-  res.json({ id: req.params.id });
+const getEmployee = async (req, res) => {
+  if (!req?.params?.id)
+    return res.status(400).json({ message: 'Employee id required' });
+
+  const employee = await Employee.findOne({ _id: req.params.id }).exec();
+  if (!employee) return res.status(404).json({ message: 'Employee not found' });
+
+  res.json(employee);
 };
 
-const createNewEmployee = (req, res) => {
-  res.status(201).json({
-    firstname: req.body.firstname,
-  });
+const createNewEmployee = async (req, res) => {
+  if (!req?.body?.firstname || !req?.body?.lastname)
+    return res.status(400).json({ message: 'Fist and last name required' });
+
+  try {
+    const result = await Employee.create({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+    });
+    res.status(201).json(result);
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-const updateEmployee = (req, res) => {
-  res.json({
-    firstname: req.body.firstname,
-  });
+const updateEmployee = async (req, res) => {
+  if (!req?.body?.id)
+    return res.status(400).json({ message: 'ID parameter is required' });
+
+  const employee = await Employee.findOne({ _id: req.body.id }).exec();
+
+  if (!employee) return res.status(404).json({ message: 'Employee not found' });
+
+  if (req.body?.firstname) employee.firstname = req.body.firstname;
+  if (req.body?.lastname) employee.lastname = req.body.lastname;
+
+  const result = await employee.save();
+  res.json(result);
 };
 
-const deleteEmployee = (req, res) => {
-  res.json({
-    id: req.body.id,
-  });
+const deleteEmployee = async (req, res) => {
+  if (!req?.body?.id)
+    return res.status(400).json({ message: 'Employee id required' });
+
+  const employee = await Employee.findOne({ _id: req.body.id }).exec();
+
+  if (!employee) return res.status(204).json({ message: 'Employee not found' });
+
+  const result = await employee.deleteOne({ _id: req.body.id });
+  res.json(result);
 };
 
 module.exports = {
--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -3,8 +3,7 @@ const verifyRoles = (...allowedRoles) => {
     if (!req?.roles) return res.sendStatus(401);
 
     const rolesArray = [...allowedRoles];
-    console.log('rolesArray', rolesArray);
-    console.log('req.roles', req.roles);
+
     const result = req.roles
       .map((role) => rolesArray.includes(role))
       .find((val) => val === true);
